c98c05d20c1a61e9748e13c8b65d69ac18e82b06
wujunyu <1318543125@qq.com>
2017-11-26 19:33:22 +0800

    singleton

---
+++ src/chapter03/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package chapter03.singleton;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+    private Singleton() {
+        System.out.println("生成一个实例。");
+    }
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
